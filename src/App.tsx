50e7e2a92b1056d7fae4cd6df554a0e118063db8
shivang <shivangaggarwal6208@gmail.com>
2026-02-09 02:31:13 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center bg-amber-50'>
+      Hello World!
+    </div>
+  )
+}
+
+export default App
