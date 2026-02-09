ec302605f33cae674c0b2a7d24f6e81332e0b31d
shivang <shivangaggarwal6208@gmail.com>
2026-02-09 20:56:01 +0530

    Structured layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,25 @@ import './App.css'
 function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-amber-50'>
-      Hello World!
+      <table className='w-1/3 h-3/4 border-collapse text-stone-900'>
+        <tbody>
+          <tr>
+            <td>X</td>
+            <td>X</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>O</td>
+            <td>O</td>
+            <td>O</td>
+          </tr>
+          <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
